aecd579f7ce94118e152a5cd09a6a4371856c56d
Benjamin Suhr <stu96681@mail.uni-kiel.de>
2015-05-17 19:06:39 +0200

    Removed potato file OceanLifeMain
    Moved Ocean to model

--- PP2015/src/control/OceanLifeMain.java
+++ PP2015/src/control/OceanLifeMain.java
@@ -1,24 +1,23 @@
 package control;
 
 import model.Fish;
-import view.Ocean;
+import model.Ocean;
 
 public class OceanLifeMain {
-	
-	public static void main (String[] args){
+
+	public static void main(String[] args) {
 		Ocean ocean;
-		
+
 		int width = 1024;
 		int depth = 768;
-		
-		Fish fish = new Fish(50,50,"Fish 1");
-	
-		ocean = new Ocean(width,depth,fish);	
+
+		Fish fish = new Fish(50, 50, "Fish 1");
+
+		ocean = new Ocean(width, depth, fish);
 		for (int i = 0; i < 5; i++) {
 			ocean.move();
 			System.out.println(ocean);
 		}
 	}
-	
 
 }

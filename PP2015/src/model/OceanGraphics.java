81167f0263e8aa02f63a9a453c4e5a9aec64c156
Tobi <tobi.gartmann@web.de>
2015-07-09 16:31:11 +0200

    Added OceanGraphics.java

---
+++ PP2015/src/model/OceanGraphics.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class OceanGraphics {
+
+    private static BufferedImage fishImage, bubbleImage, plantImage,
+	    sharkImage, stoneImage, oceanImage;
+
+    public OceanGraphics() {
+	try {
+	    fishImage = ImageIO.read(OceanGraphics.class
+		    .getResource("/res/fish (2).png"));
+	    bubbleImage = ImageIO.read(OceanGraphics.class
+		    .getResource("/res/bubble.png"));
+	    plantImage = ImageIO.read(OceanGraphics.class
+		    .getResource("/res/plant.png"));
+	    sharkImage = ImageIO.read(OceanGraphics.class
+		    .getResource("/res/shark.png"));
+	    stoneImage = ImageIO.read(OceanGraphics.class
+		    .getResource("/res/stone.png"));
+	    oceanImage = ImageIO.read(OceanGraphics.class
+		    .getResource("/res/Ocean.png"));
+	} catch (IOException e) {
+	    // Do stuff to see for user, like JOptionPane
+	}
+    }
+
+    public static BufferedImage getImage(OceanObject oceanObject) {
+	if (oceanObject.getClass().equals(Fish.class)) {
+	    return fishImage;
+	} else if (oceanObject.getClass().equals(Bubble.class)) {
+	    return bubbleImage;
+	} else if (oceanObject.getClass().equals(Plant.class)) {
+	    return plantImage;
+	} else if (oceanObject.getClass().equals(Shark.class)) {
+	    return sharkImage;
+	} else if (oceanObject.getClass().equals(Stone.class)) {
+	    return stoneImage;
+	} else {
+	    return null;
+	}
+
+    }
+
+    public static BufferedImage getOceanImage() {
+	return oceanImage;
+    }
+
+}

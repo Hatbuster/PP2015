33e0cc0f068a1779183e96cc16bb04fa0ffd930c
Benjamin Suhr <stu96681@mail.uni-kiel.de>
2015-07-09 16:52:56 +0200

    getImage now loads images when needed, instead of just returning nothing

--- PP2015/src/model/OceanGraphics.java
+++ PP2015/src/model/OceanGraphics.java
@@ -7,8 +7,8 @@ import javax.imageio.ImageIO;
 
 public class OceanGraphics {
 
-    private static BufferedImage fishImage, bubbleImage, plantImage,
-	    sharkImage, stoneImage, oceanImage;
+    private static BufferedImage fishImage, bubbleImage, plantImage, sharkImage,
+	    stoneImage, oceanImage;
 
     private static boolean loaded;
 
@@ -17,20 +17,19 @@ public class OceanGraphics {
     }
 
     public static BufferedImage getImage(OceanObject oceanObject) {
-	if (loaded) {
-	    if (oceanObject.getClass().equals(Fish.class)) {
-		return fishImage;
-	    } else if (oceanObject.getClass().equals(Bubble.class)) {
-		return bubbleImage;
-	    } else if (oceanObject.getClass().equals(Plant.class)) {
-		return plantImage;
-	    } else if (oceanObject.getClass().equals(Shark.class)) {
-		return sharkImage;
-	    } else if (oceanObject.getClass().equals(Stone.class)) {
-		return stoneImage;
-	    } else {
-		return null;
-	    }
+	if (!loaded) {
+	    loadAllImages();
+	}
+	if (oceanObject.getClass().equals(Fish.class)) {
+	    return fishImage;
+	} else if (oceanObject.getClass().equals(Bubble.class)) {
+	    return bubbleImage;
+	} else if (oceanObject.getClass().equals(Plant.class)) {
+	    return plantImage;
+	} else if (oceanObject.getClass().equals(Shark.class)) {
+	    return sharkImage;
+	} else if (oceanObject.getClass().equals(Stone.class)) {
+	    return stoneImage;
 	} else {
 	    return null;
 	}
@@ -42,18 +41,18 @@ public class OceanGraphics {
 
     public static void loadAllImages() {
 	try {
-	    fishImage = ImageIO.read(OceanGraphics.class
-		    .getResource("/res/fish (2).png"));
-	    bubbleImage = ImageIO.read(OceanGraphics.class
-		    .getResource("/res/bubble.png"));
-	    plantImage = ImageIO.read(OceanGraphics.class
-		    .getResource("/res/plant.png"));
-	    sharkImage = ImageIO.read(OceanGraphics.class
-		    .getResource("/res/shark.png"));
-	    stoneImage = ImageIO.read(OceanGraphics.class
-		    .getResource("/res/stone.png"));
-	    oceanImage = ImageIO.read(OceanGraphics.class
-		    .getResource("/res/Ocean.png"));
+	    fishImage = ImageIO
+		    .read(OceanGraphics.class.getResource("/res/fish (2).png"));
+	    bubbleImage = ImageIO
+		    .read(OceanGraphics.class.getResource("/res/bubble.png"));
+	    plantImage = ImageIO
+		    .read(OceanGraphics.class.getResource("/res/plant.png"));
+	    sharkImage = ImageIO
+		    .read(OceanGraphics.class.getResource("/res/shark.png"));
+	    stoneImage = ImageIO
+		    .read(OceanGraphics.class.getResource("/res/stone.png"));
+	    oceanImage = ImageIO
+		    .read(OceanGraphics.class.getResource("/res/Ocean.png"));
 	    loaded = true;
 	} catch (IOException e) {
 	    loaded = false;
